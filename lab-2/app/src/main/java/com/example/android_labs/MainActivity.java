5ec36d56f5da9c683af13f88e7ab2a086c1532cb
denis <miroshnichenkodenis2004@gmail.com>
2024-01-01 10:16:06 +0800

    lab 2

---
+++ lab-2/app/src/main/java/com/example/android_labs/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android_labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final TextView textView = findViewById(R.id.textView);
+        final EditText editText = findViewById(R.id.editTextText);
+        Button button = (Button) findViewById(R.id.button);
+        textView.setText("");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TextView textView = findViewById(R.id.textView);
+                final EditText editText = findViewById(R.id.editTextText);
+                Button button = (Button) findViewById(R.id.button);
+                textView.setText("");
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        textView.setText("Привет, " +
+                                editText.getText().toString() + "!");
+                    }
+                });
+            }
+        });
+    }
+
+}
