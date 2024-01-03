4a5e10032bbba743ecae32f4f84b7c93557f451e
denis <miroshnichenkodenis2004@gmail.com>
2024-01-03 10:57:14 +0800

    complete lab 9

--- lab-9/app/src/main/java/com/example/lab_9/MainActivity.java
+++ lab-9/app/src/main/java/com/example/lab_9/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.lab_9;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text1);
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.contextmenu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.color_red){
+            TextView textView = (TextView) findViewById(R.id.text1);
+            textView.setTextColor(Color.parseColor("red"));
+        }
+        if (id == R.id.color_black){
+            TextView textView = (TextView) findViewById(R.id.text1);
+            textView.setTextColor(Color.parseColor("black"));
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
@@ -31,6 +58,9 @@ public class MainActivity extends AppCompatActivity {
                 item.setChecked(true);
             }
         }
+
+        Toast.makeText(MainActivity.this, R.string.fio,
+        Toast.LENGTH_LONG).show();
         return super.onOptionsItemSelected(item);
     }
 
