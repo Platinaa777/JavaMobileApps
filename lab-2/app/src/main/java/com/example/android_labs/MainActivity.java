b81c279ceae76a8a0cc189fe423cc55583ab54d4
denis <miroshnichenkodenis2004@gmail.com>
2024-01-08 09:14:54 +0800

    change sdk ver and do task for 10 points

--- lab-2/app/src/main/java/com/example/android_labs/MainActivity.java
+++ lab-2/app/src/main/java/com/example/android_labs/MainActivity.java
@@ -11,12 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         final TextView textView = findViewById(R.id.textView);
         final EditText editText = findViewById(R.id.editTextText);
